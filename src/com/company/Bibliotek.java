aeb69f66e542f3340e072a66611302acd549f038
alexander6273 <alex6273@stud.kea.dk>
2022-02-01 15:42:51 +0100

    Bog er løst

---
+++ src/com/company/Bibliotek.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Bibliotek {
+    private ArrayList<Bog> bibliotek = new ArrayList<>();
+
+
+    public Boolean containsISBN(Bog testBog) {
+        boolean result = false;
+        for (int i = 0; i < bibliotek.size(); i++) {
+            if (bibliotek.get(i).getISBNNummer().contains(testBog.getISBNNummer())) {
+                System.out.println("True! listen indeholder allerede dette ISBN-nummer");
+                result = true;
+            }
+        } return result;
+    }
+
+    public static void main(String[] args) {
+    Bibliotek bibliotek = new Bibliotek();
+
+    Bog bog1 = new Bog("9846576531", "Original bog 1", 2001);
+    Bog bog2 = new Bog("9846576784", "Original bog 2", 2011);
+    Bog bog3 = new Bog("9846576745", "Original bog 3", 2022);
+    Bog bog4 = new Bog("98465767423", "Original bog 4", 2222);
+
+    bibliotek.bibliotek.add(bog1);
+    bibliotek.bibliotek.add(bog2);
+    bibliotek.bibliotek.add(bog3);
+
+
+    if (!bibliotek.containsISBN(bog4)){
+        System.out.println("false!");
+    }
+    bibliotek.containsISBN(bog1);
+    }
+
+}
---
+++ src/com/company/Bog.java
@@ -0,0 +1,51 @@
+package com.company;
+
+public class Bog {
+    private String ISBNNummer;
+    private String titel;
+    private int udgivelsesÅr;
+
+    public Bog(){}
+
+    public Bog(String ISBNNummer, String titel, int udgivelsesÅr){
+        this.ISBNNummer = ISBNNummer;
+        this.titel = titel;
+        this.udgivelsesÅr = udgivelsesÅr;
+    }
+
+    public String getISBNNummer() {
+        return ISBNNummer;
+    }
+
+    public void setISBNNummer(String ISBNNummer) {
+        this.ISBNNummer = ISBNNummer;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    public int getUdgivelsesÅr() {
+        return udgivelsesÅr;
+    }
+
+    public void setUdgivelsesÅr(int udgivelsesÅr) {
+        this.udgivelsesÅr = udgivelsesÅr;
+    }
+
+    @Override
+    public String toString(){
+        return "ISBN-nummer: " + ISBNNummer + " | " +
+                "Titel: " + titel  + " | " +
+                "Udgivelsesår: " + udgivelsesÅr;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
